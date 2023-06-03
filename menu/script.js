a7c93fe24fcd944046d8e191bac13a85f66a3575
Kantikkk <134126267+Kantikkk@users.noreply.github.com>
2023-06-03 11:57:53 +0600

    Add files via upload

--- menu/script.js
+++ menu/script.js
@@ -15,25 +15,3 @@ $(document).ready(function() {
         }
     });
 });
-
-
-function calculate() {
-    const height = Number(document.querySelector('.heightInput').value);
-    const weight = Number(document.querySelector('.weightInput').value);
-    const BMI = (weight / (height * height)) * 10000;
-    var finalBMI = BMI.toFixed(2);
-    var outputMessage = '';
-  
-    if (isNaN(BMI)) {
-      outputMessage = "Invalid input. Please enter numerical values for weight and height.";
-    } else if (BMI < 18.5) {
-      outputMessage = "Your BMI is " + finalBMI + ", which means you are underweight.";
-    } else if (BMI >= 18.5 && BMI <= 24.9) {
-      outputMessage = "Your BMI is " + finalBMI + ", which means you have a normal weight.";
-    } else {
-      outputMessage = "Your BMI is " + finalBMI + ", which means you might be overweight.";
-    }
-  
-    document.querySelector('.output').innerHTML = outputMessage;
-  }
-  
--- plans/script.js
+++ plans/script.js
@@ -1,77 +1,88 @@
-$(document).ready(function() {
-    $('form').submit(function(event) {
-      event.preventDefault();
-      var name = $('#name').val();
-      var surname = $('#surname').val();
-      var birthday = $('#birthday').val();
-      var email = $('#email').val();
-      var debitCard = $('#debit-card').val();
-      var debitCardDate = $('#debit-card-date').val();
-  
-      var currentDate = new Date();
-      var currentYear = currentDate.getFullYear() % 100;
-      var currentMonth = currentDate.getMonth() + 1;
-      var inputDate = new Date("20" + debitCardDate.split('/')[1], debitCardDate.split('/')[0] - 1);
-      var inputYear = inputDate.getFullYear() % 100;
-      var inputMonth = inputDate.getMonth() + 1;
-  
-      if (inputYear < currentYear || (inputYear === currentYear && inputMonth < currentMonth)) {
-        alert("Invalid debit card expiration date!");
-      } else {
-        alert("Congratulations, your are done!");
-      }   
-    });
 
-    $('form input').keypress(function(event) {
-      if (event.which === 13) {
-        event.preventDefault();
-        var $inputs = $(this).parents('form').find('input');
-        var $nextInput = $inputs.eq($inputs.index(this) + 1);
-        $nextInput.focus();
-      }
-    });
+$(document).ready(function () {
+  var today = new Date().toISOString().split('T')[0];
+  $('#birthday').attr('max', today);
+});
 
-    
-  $('form input').mousemove(function(event) {
-    var inputName = $(this).attr('name');
-    var tooltipText = '';
+$(document).ready(function () {
+  $('form').submit(function (event) {
+    event.preventDefault();
+    var name = $('#name').val();
+    var surname = $('#surname').val();
+    var birthday = $('#birthday').val();
+    var email = $('#email').val();
+    var debitCard = $('#debit-card').val();
+    var debitCardDate = $('#debit-card-date').val();
 
-    switch (inputName) {
-      case 'name':
-        tooltipText = 'Valid name format: John Doe';
-        break;
-      case 'debit-card':
-        tooltipText = 'Valid card number format: XXXX-XXXX-XXXX-XXXX';
-        break;
-      case 'debit-card-date':
-        tooltipText = 'Valid expiration date format: MM/YY';
-        break;
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear() % 100;
+    var currentMonth = currentDate.getMonth() + 1;
+    var inputDate = new Date("20" + debitCardDate.split('/')[1], debitCardDate.split('/')[0] - 1);
+    var inputYear = inputDate.getFullYear() % 100;
+    var inputMonth = inputDate.getMonth() + 1;
+
+    if (inputYear < currentYear || (inputYear === currentYear && inputMonth < currentMonth)) {
+      alert("Invalid debit card expiration date!");
+    } else {
+      alert("Congratulations, your information is correct!");
+      $('form').unbind('submit').submit();
+
     }
-
-    $('.tooltip').text(tooltipText);
-    $('.tooltip').css({
-      'top': event.pageY + 10,
-      'left': event.pageX + 10
-    });
   });
 
-  $('form input').mouseenter(function() {
-    $('.tooltip').show();
+  $('form input').keypress(function(event) {
+    if (event.which === 13) {
+      event.preventDefault();
+      var $inputs = $(this).parents('form').find('input');
+      var $nextInput = $inputs.eq($inputs.index(this) + 1);
+      $nextInput.focus();
+    }
   });
 
-  $('form input').mouseleave(function() {
-    $('.tooltip').hide();
-  });
-
-    $('.buy-button').click(function() {
-      var selectedPlan = $(this).data('plan');
-      $('#package-select').val(selectedPlan);
-      $('#package-select').addClass('is-selected');
   
-      var personalSectionOffset = $('#personal-section').offset().top;
-  
-      $('html, body').animate({
-        scrollTop: personalSectionOffset
-      }, 200);
-    });
+$('form input').mousemove(function(event) {
+  var inputName = $(this).attr('name');
+  var tooltipText = '';
+
+  switch (inputName) {
+    case 'name':
+      tooltipText = 'Valid name format: John Doe';
+      break;
+    case 'debit-card':
+      tooltipText = 'Valid card number format: XXXX-XXXX-XXXX-XXXX';
+      break;
+    case 'debit-card-date':
+      tooltipText = 'Valid expiration date format: MM/YY';
+      break;
+  }
+
+  $('.tooltip').text(tooltipText);
+  $('.tooltip').css({
+    'top': event.pageY + 10,
+    'left': event.pageX + 10
   });
+});
+
+$('form input').mouseenter(function() {
+  $('.tooltip').show();
+});
+
+$('form input').mouseleave(function() {
+  $('.tooltip').hide();
+});
+
+  $('.buy-button').click(function() {
+    var selectedPlan = $(this).data('plan');
+    $('#package-select').val(selectedPlan);
+    $('#package-select').addClass('is-selected');
+
+    var personalSectionOffset = $('#personal-section').offset().top;
+
+    $('html, body').animate({
+      scrollTop: personalSectionOffset
+    }, 200);
+  });
+});
+
+
+
